fa301f6df7daca4f874c2893ffb30fa0576dd5d5
Zhang He <zhang_he06@163.com>
2016-03-03 01:28:06 +0800

    新增主机名称替换脚本

---
+++ linux/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# 定义 host name
+host_name='ThinkPad-X240'
+
+# 设置 hosts 文件中的 host name
+sed -i "s/127.0.1.1       ubuntu  # 主机名称/127.0.1.1       ${host_name}  # 主机名称/g" hosts
+sed -i "s/127.0.1.1       ubuntu  # 主机名称/127.0.1.1       ${host_name}  # 主机名称/g" hosts.develop
+sed -i "s/127.0.1.1       ubuntu  # 主机名称/127.0.1.1       ${host_name}  # 主机名称/g" hosts.google
+sed -i "s/127.0.1.1       ubuntu  # 主机名称/127.0.1.1       ${host_name}  # 主机名称/g" hosts.original
+sed -i "s/127.0.1.1       ubuntu  # 主机名称/127.0.1.1       ${host_name}  # 主机名称/g" hosts.work
